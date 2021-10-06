fbcfb1fab870f8c06e6a000b770b0f33b82b0a38
Jack Clarke <jclar162@uottawa.ca>
2021-10-06 02:21:58 +0000

    set up header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,40 @@
+import { getImage } from "gatsby-plugin-image"
 import * as React from "react"
 import Layout from "../components/layout"
+import { Image, Container, Row, Col } from 'react-bootstrap'
+
 
 
 const IndexPage = () => {
+
+
+    const profilePicture = "https://www.101greatgoals.com/wp-content/uploads/2019/06/GettyImages-1143042384.jpg"
+    const firstName = "Jack"
+    const lastName = "Clarke"
+
     return (
         <Layout>
             <main>
-                <div className={"text-center"}>
-                    <h1>Welcome to my website!</h1>
-                    <h2> This website is a WIP, but feel free to check it out</h2>
-                </div>
+                <Container>
+                    <Row>
+                        <Col sm>
+                            <Image src={profilePicture} roundedCircle fluid></Image>
+                        </Col>
+                        <Col sm>
+                            <div class="p-5 h-100 d-flex justify-content-center align-items-center">
+                                <Container>
+                                    <Row>
+                                        <Col><h1>{firstName}</h1></Col>
+                                    </Row>
+                                    <Row>
+                                        <Col xs={{ offset: 1 }}><h1>{lastName}</h1></Col>
+                                    </Row>
+                                </Container>
+                            </div>
+                        </Col>
+                    </Row>
+                    <hr></hr>
+                </Container>
             </main>
         </Layout>
     )
