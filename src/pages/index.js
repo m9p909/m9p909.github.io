c81df6f04945be9a5015fdf5c0add2ec1d814efa
Jack Clarke <jclar162@uottawa.ca>
2021-10-11 23:11:38 +0000

    set up gatsby to fetch user data

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,26 +2,55 @@ import { getImage } from "gatsby-plugin-image"
 import * as React from "react"
 import Layout from "../components/layout"
 import { Image, Container, Row, Col } from 'react-bootstrap'
+import { useStaticQuery, graphql } from "gatsby"
 
 
 
 const IndexPage = () => {
+    const data = useStaticQuery(graphql`query GetOwner {
+        contentfulOwner {
+          lastName
+          firstName
+          profilePicture {
+            fixed(height: 400, width: 400) {
+              srcWebp
+              base64
+            }
+          }
+          experience {
+            name
+            points {
+              childrenContentfulPointDescriptionTextNode {
+                description
+              }
+            }
+          }
+          projects {
+            name
+            points {
+              childrenContentfulPointDescriptionTextNode {
+                description
+              }
+            }
+          }
+        }
+      }`);
 
-
-    const profilePicture = "https://www.101greatgoals.com/wp-content/uploads/2019/06/GettyImages-1143042384.jpg"
-    const firstName = "Jack"
-    const lastName = "Clarke"
+    const owner = data.contentfulOwner;
+    const profilePicture = owner.profilePicture.fixed.srcWebp;
+    const firstName = owner.firstName;
+    const lastName = owner.lastName;
 
     return (
         <Layout>
             <main>
                 <Container>
                     <Row>
-                        <Col sm>
+                        <Col sm className='text-center'>
                             <Image src={profilePicture} roundedCircle fluid></Image>
                         </Col>
                         <Col sm>
-                            <div class="p-5 h-100 d-flex justify-content-center align-items-center">
+                            <div className="p-5 h-100 d-flex justify-content-center align-items-center">
                                 <Container>
                                     <Row>
                                         <Col><h1>{firstName}</h1></Col>
