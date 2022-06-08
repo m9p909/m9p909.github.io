e05e5ae5abe2b72b6a5db1c407a74675998e05b8
Jack Clarke <jclar162@uottawa.ca>
2022-06-08 23:27:05 +0000

    stop using pixi a bit

--- src/components/CoolAnimation.js
+++ src/components/CoolAnimation.js
@@ -1,14 +1,11 @@
 import assert from "../utils/assert";
 import { ButtonGroup, ToggleButton } from "react-bootstrap";
 import React from "react";
-import { Graphics, Stage } from "react-pixi-fiber";
 
 
 
 export default function CoolAnimation(props) {
 
-    return (<Stage>
-        <Graphics></Graphics>
-    </Stage>)
+    return (<></>)
 
 }
